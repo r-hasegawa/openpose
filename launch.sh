8eb7a235c2dd3319c9b5013b723ff0d3d152effe
r-hasegawa <r-hasegawa@ist.osaka-u.ac.jp>
2021-02-18 13:10:04 +0900

    終了時コンテナ削除

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 sudo xhost + local:root
-sudo docker run -it --gpus all -e DISPLAY=$DISPLAY --net host -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $HOME/.Xauthority:/root/.Xauthority  --device=/dev/video0:/dev/video0:mwr -v $HOME/openpose:/openpose -v $HOME/data:/data cnjoads/openpose_staf:20201211 /bin/bash
+sudo docker run -it --rm --gpus all -e DISPLAY=$DISPLAY --net host -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $HOME/.Xauthority:/root/.Xauthority  --device=/dev/video0:/dev/video0:mwr -v $HOME/openpose:/openpose -v $HOME/data:/data cnjoads/openpose_staf:20201211 /bin/bash
